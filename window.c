b6404b391ff506e56fb6744d8a49f94e45cdc758
gabrielconstantin999999-lgtm <gabriel.constantin999999@gmail.com>
2026-03-31 21:09:46 +0200

    made draw polygon function

--- window.c
+++ window.c
@@ -4,34 +4,30 @@
 #define HEIGHT 600
 
 
+typedef struct {
+        double x, y;
+    }vec2;
+
+typedef struct {
+        double x, y, z;
+    }vec3;
 
 void draw_line(double x1, double y1, double x2, double y2, SDL_Renderer *renderer);
+void draw_polygon(vec2* points, int count, SDL_Renderer* renderer);
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *win = SDL_CreateWindow("engine", WIDTH, HEIGHT, 0);
+    SDL_Window *win = SDL_CreateWindow("3D Game Engine", WIDTH, HEIGHT, 0);
     SDL_Renderer *ren = SDL_CreateRenderer(win, NULL);
     SDL_Event e;
-
-    typedef struct {
-        double x, y;
-    }vec2;
-
-    typedef struct {
-        double x, y, z;
-    }vec3;
-
-    SDL_FPoint points[5] = {
-        {400.0f, 300.0f},
-        {401.0f, 300.0f},
-        {399.0f, 300.0f},
-        {400.0f, 301.0f},
-        {400.0f, 299.0f},
+    vec2 points[] = {
+        {200.0f, 200.0f},
+        {200.0f, 500.0f},
+        {400.0f, 500.0f},
+        {400.0f, 200.0f}
     };
 
-    vec2 p1 = {200.0f, 200.0f};
-    vec2 p2 = {500.0f, 500.0f};
 
     int running = 1;
 
@@ -44,10 +40,8 @@ int main() {
         SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
 
 
-        SDL_RenderPoints(ren, points, 5);
-        //SDL_RenderLine(ren, p1.x,p1.x,p2.x,p2.y);
-        draw_line(p1.x,p1.x,p2.x,p2.y, ren);
-
+        draw_polygon(points, 4, ren);
+        
 
         SDL_RenderPresent(ren);
         SDL_Delay(16);
@@ -58,6 +52,12 @@ int main() {
     return 0;
 }
 
+
+
+
+
+
+
 void draw_line(double x1, double y1, double x2, double y2, SDL_Renderer *renderer){
     for (double t = 0; t <= 1; t += 0.001f){
         double x = x1 + t * (x2 - x1);
@@ -65,3 +65,16 @@ void draw_line(double x1, double y1, double x2, double y2, SDL_Renderer *rendere
         SDL_RenderPoint(renderer, x, y);
     }
 }
+
+void draw_polygon(vec2* points, int count, SDL_Renderer* renderer){
+    if (count < 2){
+        return;
+    }
+    for (int i = 0; i < count; i += 1){
+        vec2 a = points[i];
+        vec2 b = points[(i + 1) % count];
+        draw_line(a.x,a.y,b.x,b.y,renderer);
+    }
+}
+
+
